68e213026e48d11ef53aef734010117b50140636
jteng2127-lab603 <jteng2127@gmail.com>
2023-05-29 23:12:19 +0800

    feat: complete code page

--- components/InputEmail.tsx
+++ components/InputEmail.tsx
@@ -2,6 +2,11 @@ import React from 'react'
 import { Button, Input, message } from 'antd'
 import { dataStore } from '@/models/data'
 import { observer } from 'mobx-react'
+import styled from 'styled-components'
+
+const Description = styled.p`
+  margin: 16px 0;
+`
 
 const InputEmail = () => {
   const sendEmail = () => {
@@ -13,11 +18,17 @@ const InputEmail = () => {
 
   return (
     <div>
-      <Input placeholder="請輸入您的電子郵件" onChange={(e) => dataStore.setEmail(e.target.value)} />
+      <Description>您可以輸入您的電子郵件，當影片生成完成後會寄到您的信箱</Description>
+      <Input
+        placeholder="請輸入您的電子郵件"
+        onChange={(e) => dataStore.setEmail(e.target.value)}
+        style={{ width: '300px' }}
+      />
+      <br />
       <Button
         type="primary"
         size="large"
-        style={{ margin: '50px 0' }}
+        style={{ margin: '16px 0' }}
         onClick={sendEmail}>寄送電子郵件</Button>
     </div>
   )
--- pages/[code].tsx
+++ pages/[code].tsx
@@ -1,11 +1,44 @@
 import React from 'react'
+import { useRouter } from 'next/router'
+import { Typography } from 'antd'
 import InputEmail from '@/components/InputEmail'
 import BaseLayout from '@/layouts/BaseLayout'
+import styled from 'styled-components'
+
+const { Text, Title } = Typography
+
+const Container = styled.div`
+  height: calc(100vh - 131px - 128px);
+  text-align: center;
+  width: 100%;
+`
+
+const GenTitle = styled(Title)`
+  margin: 128px 0;
+`
+
+const GenSubTitle = styled.h2`
+  margin: 64px 0 16px;
+`
+
+const GenLink = styled.div`
+  margin: 16px 0 64px !important;
+`
 
 const VideoProgress = () => {
+  const router = useRouter()
+  const { code } = router.query
+
   return (
     <BaseLayout>
-      <InputEmail />
+      <Container>
+        <GenTitle>您的影片正在生成中！</GenTitle>
+        <GenSubTitle>您可以透過以下連結查看影片生成進度</GenSubTitle>
+        <GenLink>
+          <Text copyable>{`${process.env.NEXT_PUBLIC_API_URL}/${code}`}</Text>
+        </GenLink>
+        <InputEmail />
+      </Container>
     </BaseLayout>
   )
 }
