471a7ec4b5d529b2b6709085e81bf0fec7784e66
jteng2127-lab603 <jteng2127@gmail.com>
2023-05-29 22:14:54 +0800

    refactor: extract AvatarDragArea style

--- components/AdjustPicturePosition.tsx
+++ components/AdjustPicturePosition.tsx
@@ -7,45 +7,46 @@ import 'video-react/dist/video-react.css'
 import { dataStore } from '@/models/data'
 import { observer } from 'mobx-react'
 import 'node_modules/video-react/dist/video-react.css'
+import styled from 'styled-components'
 
 const AdjustPicturePosition = () => {
   const onDrag = (e: DraggableEvent, ui: DraggableData) => {
     dataStore.setAvatarPosition([ui.x, ui.y])
   }
 
+  const AvatarDragArea = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+    pointer-events: none;
+  `
   return (
-    <Row style={{ padding: 24, height: 500, display: 'inline-block' }}>
-      <Col span={24} style={{ backgroundColor: '#f0f2f5' }}>
+    <Row>
+      <Col span={24} style={{ position: 'relative'}}>
         <Player
           autoPlay
           muted
           src={dataStore.videoBase64 ?? undefined}
-          fluid={false}
-          height={500} >
+          fluid={true}
+        >
         </Player>
-        <div
-          style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            width: '100%',
-            height: '100%',
-            zIndex: 1,
-            pointerEvents: 'none'
-          }}>
+        <AvatarDragArea>
           <Draggable
             bounds="parent"
             onDrag={onDrag} >
             <Avatar
               src={dataStore.avatarBase64 ?? undefined}
               icon={<UserOutlined />}
-              size={150}
+              size={256}
               shape={dataStore.avatarShape}
               draggable={false}
               style={{ pointerEvents: 'auto', backgroundColor: 'rgba(0, 0, 0, 0.5)' }}
             />
           </Draggable>
-        </div>
+        </AvatarDragArea>
       </Col>
     </Row>
   )
