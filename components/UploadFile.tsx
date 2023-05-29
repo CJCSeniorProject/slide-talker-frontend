c02d43a7bf802da613e75142611177bcdcda3d1c
jteng2127-lab603 <jteng2127@gmail.com>
2023-05-29 22:14:54 +0800

    feat: delete anchor in main, combine UploadFile into one

---
+++ components/UploadFile.tsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import { InboxOutlined } from '@ant-design/icons'
+import { message, Upload } from 'antd'
+import { observer } from 'mobx-react'
+import { dataStore } from '@/models/data'
+
+const { Dragger } = Upload
+
+const UploadFile = ({uploadType}: {uploadType: 'video'|'avatar'}) => {
+  let dataStoreSetter: (file: File) => void
+  let accept: string
+  let uploadText: string
+  let uploadHint: string
+
+  if (uploadType === 'video') {
+    dataStoreSetter = (file: File) => dataStore.setVideo(file)
+    accept = 'video/mp4, video/quicktime'
+    uploadText = '上傳影片檔案'
+    uploadHint = 'Drag or click to upload video file'
+  }
+  else {
+    dataStoreSetter = (file: File) => dataStore.setAvatar(file)
+    accept = 'image/jpeg, image/png'
+    uploadText = '上傳頭像檔案'
+    uploadHint = 'Drag or click to upload avatar file'
+  }
+
+  const handleUpload = async (option: any) => {
+    const file = option.file as File
+    try {
+      dataStoreSetter(file)
+      option.onSuccess()
+      message.success(`${file.name} file uploaded successfully`)
+    }
+    catch (error) {
+      console.log(error)
+      option.onError()
+      message.error(`${file.name} file upload failed.`)
+    }
+  }
+
+  const beforeUpload = (file: File) => {
+    const maxMB = 100
+    const isLtMB = file.size / 1024 / 1024 < maxMB
+    if (!isLtMB) {
+      message.error(`File must smaller than ${maxMB}MB!`)
+    }
+    return isLtMB
+  }
+  
+  return (
+    <Dragger
+      name="file"
+      maxCount={1}
+      customRequest={handleUpload}
+      accept={accept}
+      beforeUpload={beforeUpload} >
+      <p className="ant-upload-drag-icon">
+        <InboxOutlined />
+      </p>
+      <p className="ant-upload-text">{uploadText}</p>
+      <p className="ant-upload-hint">{uploadHint}</p>
+    </Dragger>
+  )
+}
+
+export default observer(UploadFile)
--- pages/main.tsx
+++ pages/main.tsx
@@ -2,11 +2,9 @@ import React from 'react'
 import { observer } from 'mobx-react-lite'
 import { Button } from 'antd'
 import { Row, Col } from 'antd'
-import { Anchor } from 'antd'
 import { Typography } from 'antd'
-import DragToUploadVideo from '@/components/DragToUploadVideo'
+import UploadFile from '@/components/UploadFile'
 import AdjustPicturePosition from '@/components/AdjustPicturePosition'
-import UploadAvatar from '@/components/UploadAvatar'
 import { dataStore } from '@/models/data'
 import BaseLayout from '@/layouts/BaseLayout'
 import HeroSection from '@/components/HeroSection'
@@ -14,61 +12,32 @@ import HeroSection from '@/components/HeroSection'
 const { Text } = Typography
 
 const Main = () => {
-  const anchorItems = [
-    {
-      key: 'slide-upload',
-      title: 'step 1',
-      href: '#slide-upload',
-    },
-    {
-      key: 'slide-record',
-      title: 'step 2',
-      href: '#slide-record',
-    },
-    {
-      key: 'slide-avator',
-      title: 'step 3',
-      href: '#slide-avator',
-    },
-  ]
-
-
   return (
     <BaseLayout>
       <HeroSection />
-      <Row>
-        <Col span={2} style={{ display: 'fix', marginTop: '200px' }}>
-          {/* TODO css useWindowSize */}
-          {/* <Anchor items={anchorItems} offsetTop={ useWindowSize().height / 2 - 60 }/> */}
-          <Anchor items={anchorItems} />
-        </Col>
-        <Col span={22}>
-          <div id="slide-upload" style={{ padding: '30px', margin: '128px 0', height: '100vh' }}>
-            <DragToUploadVideo />
-          </div>
-          {/* TODO: record voice */}
-          {/* <div id="slide-record" style={{ padding: '50px 30px', margin: '128px 0', height: '100vh' }}>
-              <SlideShow />
-              <RecordVoice />
-            </div> */}
-          <div id="slide-avator" style={{ padding: '50px 30px', margin: '128px 0', height: '100vh' }}>
-            <Row gutter={20}>
+      <div id="slide-avator" style={{ height: '100vh', background: 'red' }}>
+        <Row gutter={20}>
+          <Col span={16}>
+            <AdjustPicturePosition />
+          </Col>
+          <Col span={8}>
+            <Row>
               <Col span={12}>
-                <AdjustPicturePosition />
+                <UploadFile uploadType='video'/>
               </Col>
               <Col span={12}>
-                <UploadAvatar />
+                <UploadFile uploadType='avatar'/>
               </Col>
             </Row>
-          </div>
-          <Button
-            type="primary"
-            size="large"
-            style={{ margin: '50px 0' }}
-            onClick={() => dataStore.post()}>上傳資料</Button>
-          <Text>{dataStore.code}</Text>
-        </Col>
-      </Row>
+            <Button
+              type="primary"
+              size="large"
+              style={{ margin: '50px 0' }}
+              onClick={() => dataStore.post()}>上傳資料</Button>
+            <Text>{dataStore.code}</Text>
+          </Col>
+        </Row>
+      </div>
     </BaseLayout>
   )
 }
